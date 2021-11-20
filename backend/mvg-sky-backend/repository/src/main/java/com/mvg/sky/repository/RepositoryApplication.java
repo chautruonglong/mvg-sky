21da182e0cb1638d4eafde0ba7939588a1259d1c
chautruonglong <truonglongchau@gmail.com>
2021-11-20 22:11:00 +0700

    Refactor dependecies & struct app

---
+++ backend/mvg-sky-backend/repository/src/main/java/com/mvg/sky/repository/RepositoryApplication.java
@@ -0,0 +1,7 @@
+package com.mvg.sky.repository;
+
+public class RepositoryApplication {
+    public static void main(String[] args) {
+        System.out.println("This is repository module");
+    }
+}
---
+++ backend/mvg-sky-backend/repository/src/test/java/com/mvg/sky/repository/RepositoryApplicationTest.java
@@ -0,0 +1,7 @@
+package com.mvg.sky.repository;
+
+public class RepositoryApplicationTest {
+    public static void main(String[] args) {
+        System.out.println("This is repository testing module");
+    }
+}
