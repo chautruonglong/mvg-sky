abcd0383824569d8db56841fa38dd3f1ef840c66
Nguyen Phuoc Quoc <phuocquocnguyen@gmail.com>
2021-11-29 06:53:48 +0700

    Mail screen and new mail screnn

---
+++ frontend/mobile/screens/AddMailScreen.js
@@ -0,0 +1,199 @@
+import React, { useEffect, useContext, useState } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    TextInput,
+    StyleSheet,
+    Alert,
+} from 'react-native';
+
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FormButton from '../components/FormButton';
+
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
+
+const AddMailScreen = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.container}>
+                <BottomSheet
+                    snapPoints={[330, -5]}
+                    initialSnap={1}
+                />
+                <Animated.View
+                    style={{
+                        margin: 20,
+                    }}>
+                    <View style={{ alignItems: 'center' }}>
+
+                        <Text style={{ marginTop: 10, fontSize: 18, fontWeight: 'bold' }}>
+                        </Text>
+                        {/* <Text>{user.uid}</Text> */}
+                    </View>
+
+                    <View style={styles.action}>
+                        <FontAwesome name="user-o" color="#333333" size={20} />
+                        <TextInput
+                            placeholder="To"
+                            placeholderTextColor="#666666"
+                            autoCorrect={false}
+                            // value={userData ? userData.fname : ''}
+                            // onChangeText={(txt) => setUserData({ ...userData, fname: txt })}
+                            style={styles.textInput}
+                        />
+                    </View>
+                    <View style={styles.action}>
+                        <FontAwesome name="user-o" color="#333333" size={20} />
+                        <TextInput
+                            placeholder="Cc/Bcc"
+                            placeholderTextColor="#666666"
+                            // value={userData ? userData.lname : ''}
+                            // onChangeText={(txt) => setUserData({ ...userData, lname: txt })}
+                            autoCorrect={false}
+                            style={styles.textInput}
+                        />
+                    </View>
+                    <View style={styles.action}>
+                        <Ionicons name="ios-clipboard-outline" color="#333333" size={20} />
+                        <TextInput
+                            multiline
+                            numberOfLines={3}
+                            placeholder="Subject"
+                            placeholderTextColor="#666666"
+                            // value={userData ? userData.about : ''}
+                            // onChangeText={(txt) => setUserData({ ...userData, about: txt })}
+                            autoCorrect={true}
+                            style={[styles.textInput, { height: 40 }]}
+                        />
+                    </View>
+                    <View style={styles.action1}>
+                        {/* <Feather name="phone" color="#333333" size={20} /> */}
+                        <TextInput
+                            placeholder="Body"
+                            placeholderTextColor="#666666"
+                            keyboardType="number-pad"
+                            autoCorrect={false}
+                            // value={userData ? userData.phone : ''}
+                            // onChangeText={(txt) => setUserData({ ...userData, phone: txt })}
+
+                            multiline={true}
+                            numberOfLines={5}
+                            style={styles.textInput1}
+                        />
+                    </View>
+                    <FormButton buttonTitle="Send"
+                    // onPress={handleUpdate} 
+                    />
+                </Animated.View>
+            </View>
+        </View>
+    );
+};
+
+export default AddMailScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    commandButton: {
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: '#FF6347',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    panel: {
+        padding: 20,
+        backgroundColor: '#FFFFFF',
+        paddingTop: 20,
+        width: '100%',
+    },
+    header: {
+        backgroundColor: '#FFFFFF',
+        shadowColor: '#333333',
+        shadowOffset: { width: -1, height: -3 },
+        shadowRadius: 2,
+        shadowOpacity: 0.4,
+        paddingTop: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    panelHeader: {
+        alignItems: 'center',
+    },
+    panelHandle: {
+        width: 40,
+        height: 8,
+        borderRadius: 4,
+        backgroundColor: '#00000040',
+        marginBottom: 10,
+    },
+    panelTitle: {
+        fontSize: 27,
+        height: 35,
+    },
+    panelSubtitle: {
+        fontSize: 14,
+        color: 'gray',
+        height: 30,
+        marginBottom: 10,
+    },
+    panelButton: {
+        padding: 13,
+        borderRadius: 10,
+        backgroundColor: '#2e64e5',
+        alignItems: 'center',
+        marginVertical: 7,
+    },
+    panelButtonTitle: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5,
+    },
+    action1: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5,
+        height: 200
+    },
+
+    actionError: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#FF0000',
+        paddingBottom: 5,
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        paddingLeft: 10,
+        color: '#333333',
+    },
+    textInput1: {
+        color: '#333333',
+        paddingRight: 30,
+        lineHeight: 23,
+        flex: 1,
+        textAlignVertical: 'top',
+        marginLeft: 20,
+    },
+});
---
+++ frontend/mobile/screens/MailScreen.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCoffee, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { AuthContext } from '../navigation/AuthProvider';
+import ActionButton from 'react-native-action-button';
+import {
+  Container,
+  Card,
+  UserInfo,
+  UserImgWrapper,
+  UserImg,
+  UserInfoText,
+  UserName,
+  PostTime,
+  MailText,
+  TextSection,
+} from '../styles/MessageStyles';
+
+// const validIcon = parseIconFromClassName('angle-double-right')
+
+const Mail = [
+  {
+    id: '1',
+    userName: 'Jenny Doe',
+    userImg: require('../assets/users/mail_1.png'),
+    emailSubject: '[Jira]',
+    mailTime: 'Mar 30',
+    mailBody:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '2',
+    userName: 'John Doe',
+    userImg: require('../assets/users/mail_2.png'),
+    emailSubject: '[Register]',
+    mailTime: 'Mar 30',
+    mailBody:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '3',
+    userName: 'Ken William',
+    userImg: require('../assets/users/mail_3.png'),
+    emailSubject: '[Login]',
+    mailTime: 'Mar 30',
+    mailBody:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '4',
+    userName: 'Selina Paul',
+    userImg: require('../assets/users/mail_4.png'),
+    emailSubject: '[Chat]',
+    mailTime: 'Mar 30',
+    mailBody:
+      'Hey there, this is my test for a post of my social app in React Native.asdadadasdadasdasdasdadadasdadadasdadadadasdadasdadas',
+  },
+  {
+    id: '5',
+    userName: 'Christy Alex',
+    userImg: require('../assets/users/mail_5.png'),
+    emailSubject: '[Call]',
+    mailTime: 'Mar 2',
+    mailBody:
+      'Hey there, this is my test for a post of my social app in React Native',
+  },
+];
+
+const MessagesScreen = ({ navigation }) => {
+
+  return (
+    <Container>
+      <FlatList
+        data={Mail}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Card
+          // onPress={() => navigation.navigate('Chat', { userName: item.userName, mailTime: item.mailTime, emailSubject: item.emailSubject, mailBody: item.mailBody })}
+          >
+            <UserInfo>
+              <UserImgWrapper>
+                <UserImg source={item.userImg} />
+              </UserImgWrapper>
+              <TextSection>
+                <UserInfoText>
+                  <UserName>
+                    <FontAwesomeIcon style={{ color: '#D2691E' }} icon={faAngleDoubleRight} />
+                    {item.userName}</UserName>
+                  <PostTime>{item.mailTime}</PostTime>
+                </UserInfoText>
+                <Text>{item.emailSubject}</Text>
+
+                <MailText
+                  numberOfLines={1}
+                >{item.mailBody}</MailText>
+
+              </TextSection>
+            </UserInfo>
+          </Card>
+        )}
+
+      />
+      <ActionButton
+        buttonColor="#2e64e5"
+        title="Add Mail"
+        onPress={() => navigation.navigate('AddMail')}>
+      </ActionButton>
+    </Container>
+  );
+};
+
+export default MessagesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mainConatinerStyle: {
+    flexDirection: 'column',
+    flex: 1
+  }, floatingMenuButtonStyle: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    bottom: 35
+  }
+});
