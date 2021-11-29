24f6d75cca4006845f772739d879419d7fc203c3
chautruonglong <truonglongchau@gmail.com>
2021-11-29 21:02:32 +0700

    Add postgres to docker compose

--- backend/mvg-sky-backend/build.sh
+++ backend/mvg-sky-backend/build.sh
@@ -2,6 +2,12 @@
 
 set -x
 
+export REGISTRY=http://mvg-sky-service-discovery:8000/eureka/
+export PG_DB=mvg-sky
+export PG_URL=jdbc:postgresql://mvg-sky-postgres:5432/${PG_DB}
+export PG_USERNAME=postgres
+export PG_PASSWORD=Ctlbi@0775516337
+
 ./mvnw clean
 ./mvnw package -Dmaven.test.skip=true \
   -pl service-discovery \
