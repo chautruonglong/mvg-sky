27222d191ecce24d9289c8356a191795aa06fe28
chautruonglong <truonglongchau@gmail.com>
2021-12-16 15:22:39 +0700

    Enable CORS for all services

---
+++ backend/mvg-sky-backend/service-document/src/main/java/com/mvg/sky/document/configuration/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package com.mvg.sky.document.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import java.util.ArrayList;
+import java.util.List;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "http://api.mvg-sky.com/api"),
+        @Server(url = "https://api.mvg-sky.com/api"),
+        @Server(url = "http://localhost:8080/api"),
+        @Server(url = "https://localhost:8080/api"),
+    },
+    info = @Info(title = "Document Service", version = "v1.0", description = "API Document - Local Port: 8005")
+)
+@SecurityScheme(name = "auth", type = SecuritySchemeType.HTTP)
+public class SwaggerConfiguration {
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        return new ArrayList<>(List.of(
+            GroupedOpenApi.builder().pathsToMatch("/ngu").group("ngu").build(),
+            GroupedOpenApi.builder().pathsToMatch("/ngu22").group("ngu22").build()
+      ));
+    }
+}
