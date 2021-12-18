c7d9efcb03003e07522c6a3fd533872701452021
chautruonglong <truonglongchau@gmail.com>
2021-12-18 09:34:45 +0700

    Config nginx for websocket

--- backend/mvg-sky-backend/admin-portal/src/main/resources/static/js/script.js
+++ backend/mvg-sky-backend/admin-portal/src/main/resources/static/js/script.js
@@ -1,5 +1,16 @@
 'use strict'
 
+// import AbstractXHRObject from 'sockjs-client/lib/transport/browser/abstract-xhr';
+
+const _start = AbstractXHRObject.prototype._start;
+
+AbstractXHRObject.prototype._start = function(method, url, payload, opts) {
+    if (!opts) {
+        opts = { noCredentials : true };
+    }
+    return _start.call(this, method, url, payload, opts);
+};
+
 let stompClient
 let username
 
@@ -12,6 +23,7 @@ const connect = (event) => {
 
         const chatPage = document.querySelector('#chat-page')
         chatPage.classList.remove('hide')
+
 
         const socket = new SockJS('http://api.mvg-sky.com/api/chats/ws')
         stompClient = Stomp.over(socket)
